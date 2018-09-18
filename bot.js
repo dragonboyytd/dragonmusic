c0d2b286ce8e37600e87d3920b8e3253cbfe45f0
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-18 05:46:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,31 @@
-const { Client, Util } = require('discord.js');
-const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./help');
+const Discord = require('discord.js');
+const db = require('quick.db');
+const client = new Discord.Client();   
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl'); 
+const translate = require('google-translate-api'); 
+const fs = require("fs");      
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");  
+const { Client, Util } = require('discord.js');  
+const UserBlocked = new Set();   
+const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const queue = new Map();
+const zalgo = require('zalgolize');   
+const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+ const dateFormat = require('dateformat');
+ const pretty = require('pretty-ms') 
+,ti={}  
+,spee={};
 
-const client = new Client({ disableEveryone: true });
-
-const youtube = new YouTube(GOOGLE_API_KEY);
-
-const queue = new Map();
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-client.on('ready', () => console.log('Yo this ready!'));
-
-client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
-
-client.on('reconnecting', () => console.log('I am reconnecting now!'));
 
 const prefix = "/"
 client.on('message', async msg => { 
@@ -230,4 +238,4 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
 });
 
-client.login(process.env.BOT_TOKEN);
+clinet.login(process.env.BOT_TOKEN);
